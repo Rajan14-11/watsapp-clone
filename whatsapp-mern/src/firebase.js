81bcdbffb2d3d736985fb3d355030b2240e97043
rajan <rajangoyal125@gmail.com>
2022-09-01 19:51:32 +0530

    frontend completed

---
+++ whatsapp-mern/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAA8XYezwhg2A076AOtvs8xOhI_SnTKLGs",
+  authDomain: "whatsapp-clone-5f8f1.firebaseapp.com",
+  projectId: "whatsapp-clone-5f8f1",
+  storageBucket: "whatsapp-clone-5f8f1.appspot.com",
+  messagingSenderId: "928127219085",
+  appId: "1:928127219085:web:4eaebc6ffc89e92535b9c4",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
